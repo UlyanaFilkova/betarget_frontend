0744896a9fe0aa6a6fcfc4c3f60cfb3427f0ffd4
Ulyana Filkova <ulyanafilkova56@gmail.com>
2024-07-11 16:29:54 +0300

    do appearance of login page

--- src/components/LoginComponent.vue
+++ src/components/LoginComponent.vue
@@ -1,34 +1,60 @@
 <script setup>
 import "@/assets/css/login.css";
+import { reactive } from "vue";
+import router from "@/router";
+import { RouterLink } from "vue-router";
+import axios from "axios";
+
+const form = reactive({
+  email: "",
+  password: "",
+});
+
+const handleSubmit = async () => {
+  const userData = {
+    email: form.email,
+    password: form.password,
+  };
+
+  try {
+    console.log(userData);
+    // const response = await axios.post("/api/jobs/", userData);
+    // router.push(`/jobs/${response.data.id}`);
+  } catch (error) {
+    console.error("Error login", error);
+  }
+};
 </script>
 
 <template>
   <section class="login">
-    <h1 class="login__logo">Betarget</h1>
-    <form class="login__form">
-      <input
-        class="login__input"
-        type="email"
-        id="login__email"
-        name="login__email"
-        placeholder="Электронная почта"
-        required
-      />
-
-      <input
-        class="login__input"
-        type="password"
-        id="login__password"
-        name="login__password"
-        placeholder="Пароль"
-        required
-      />
-
-      <button type="submit">Вход</button>
-    </form>
-    <p>
-      <a href="#">Забыли пароль?</a>
-      <a href="#">Создать аккаунт</a>
-    </p>
+    <div class="login__wrapper">
+      <h1 class="login__logo">Betarget</h1>
+      <form class="login__form" @submit.prevent="handleSubmit">
+        <input
+          class="login__input"
+          type="email"
+          id="login__email"
+          name="login__email"
+          v-model="form.email"
+          placeholder="Электронная почта"
+          required
+        />
+        <input
+          class="login__input"
+          type="password"
+          id="login__password"
+          name="login__password"
+          v-model="form.password"
+          placeholder="Пароль"
+          required
+        />
+        <button class="login__button" type="submit">Вход</button>
+        <div class="login__links">
+          <RouterLink to="" class="login__link">Забыли пароль?</RouterLink>
+          <RouterLink to="" class="login__link">Создать аккаунт</RouterLink>
+        </div>
+      </form>
+    </div>
   </section>
 </template>
