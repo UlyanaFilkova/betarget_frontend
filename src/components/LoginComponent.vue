3427ffd598290bd48e8b7190821dd937f245ae9d
Ulyana Filkova <ulyanafilkova56@gmail.com>
2024-07-12 09:13:50 +0300

    add email and password validation to login page

--- src/components/LoginComponent.vue
+++ src/components/LoginComponent.vue
@@ -1,6 +1,6 @@
 <script setup>
 import "@/assets/css/login.css";
-import { reactive } from "vue";
+import { reactive, ref, onMounted, computed } from "vue";
 import router from "@/router";
 import { RouterLink } from "vue-router";
 import axios from "axios";
@@ -8,6 +8,53 @@ import axios from "axios";
 const form = reactive({
   email: "",
   password: "",
+});
+
+const loginEmail = ref(null);
+const loginPassword = ref(null);
+
+const errors = reactive({
+  email: "",
+});
+
+function validatePassword(password) {
+  const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+  return passwordRegex.test(password);
+}
+
+onMounted(() => {
+  loginEmail.value.addEventListener("focusout", (event) => {
+    if (loginEmail.value.validity.typeMismatch) {
+      errors.email = "Введите корректный email";
+      loginEmail.value.addEventListener("input", (event) => {
+        if (loginEmail.value.validity.typeMismatch) {
+          errors.email = "Введите корректный email";
+        } else {
+          errors.email = "";
+        }
+      });
+    } else {
+      errors.email = "";
+    }
+  });
+
+  loginPassword.value.addEventListener("focusout", (event) => {
+    console.log(loginPassword.value.value);
+    if (!validatePassword(loginPassword.value.value)) {
+      errors.password =
+        "Пароль должен быть не менее 8 символов и содержать большие буквы и цифры";
+      loginPassword.value.addEventListener("input", (event) => {
+        if (!validatePassword(loginPassword.value.value)) {
+          errors.password =
+            "Пароль должен быть не менее 8 символов и содержать большие буквы и цифры";
+        } else {
+          errors.password = "";
+        }
+      });
+    } else {
+      errors.password = "";
+    }
+  });
 });
 
 const handleSubmit = async () => {
@@ -30,7 +77,7 @@ const handleSubmit = async () => {
   <section class="login">
     <div class="login__wrapper">
       <h1 class="login__logo">Betarget</h1>
-      <form class="login__form" @submit.prevent="handleSubmit">
+      <form novalidate class="login__form" @submit.prevent="handleSubmit">
         <input
           class="login__input"
           type="email"
@@ -38,8 +85,15 @@ const handleSubmit = async () => {
           name="login__email"
           v-model="form.email"
           placeholder="Электронная почта"
+          autocomplete="email"
+          ref="loginEmail"
           required
         />
+        <span
+          :style="{ visibility: errors.email ? 'visible' : 'hidden' }"
+          class="login__error"
+          >{{ errors.email }}</span
+        >
         <input
           class="login__input"
           type="password"
@@ -47,8 +101,15 @@ const handleSubmit = async () => {
           name="login__password"
           v-model="form.password"
           placeholder="Пароль"
+          autocomplete="current-password"
+          ref="loginPassword"
           required
         />
+        <span
+          :style="{ visibility: errors.password ? 'visible' : 'hidden' }"
+          class="login__error"
+          >{{ errors.password }}</span
+        >
         <button class="login__button" type="submit">Вход</button>
       </form>
       <div class="login__links">
