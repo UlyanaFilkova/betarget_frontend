b8390ee12a30dc301874acaa391ebb03cfb807b8
Ulyana Filkova <ulyanafilkova56@gmail.com>
2024-07-18 21:21:11 +0300

    view active resume in console.dir

--- src/components/CrmComponents/resumeComponents/Resume.vue
+++ src/components/CrmComponents/resumeComponents/Resume.vue
@@ -2,7 +2,6 @@
 import ResumeActions from "@/components/crmComponents/resumeComponents/ResumeActions.vue";
 import ResumeContent from "@/components/crmComponents/resumeComponents/ResumeContent.vue";
 import ResumeInfo from "@/components/crmComponents/resumeComponents/ResumeInfo.vue";
-
 import {
   reactive,
   ref,
@@ -18,24 +17,30 @@ import Resume from "@/models/resume.js";
 import axios from "axios";
 
 const store = useStore();
-let activeResumeId = computed(() => Number(store.state.activeResume));
+let activeResumeId = computed(() => Number(store.state.activeResumeId));
+let resume = reactive({});
 
 watch(activeResumeId, (newActiveResumeId) => {
   let resumes = computed(() => store.state.resumes);
   resumes = toRaw(unref(resumes));
-  let resume = resumes.find((resume) => resume.id === newActiveResumeId);
-  resume = toRaw(resume);
+
+  let resumeData = resumes.find((resume) => resume.id === newActiveResumeId);
+  console.dir(resumeData);
+  resumeData = toRaw(resumeData);
+  console.dir(resumeData);
+  resume = new Resume(resumeData);
   console.dir(resume);
+  store.dispatch("updateActiveResume", toRaw(resume));
 });
 </script>
 
 <template>
   <div class="resume">
     <section class="resume-display">
-      <ResumeActions />
-      <ResumeContent />
+      <ResumeActions :resumeData="resume" />
+      <ResumeContent :resume="resume" />
     </section>
-    <ResumeInfo />
+    <ResumeInfo :resume="resume" />
 
     <div class="resume__not-chosen">Выберите резюме</div>
   </div>
--- src/store.js
+++ src/store.js
@@ -5,23 +5,29 @@ const store = createStore({
   state: {
     activeVacancy: null,
     activeResume: null,
+    activeResumeId: null,
     resumes: null,
   },
   mutations: {
     updateActiveVacancy(state, activeVacancy) {
       state.activeVacancy = activeVacancy;
     },
+    updateActiveResumeId(state, activeResumeId) {
+      state.activeResumeId = activeResumeId;
+    },
     updateActiveResume(state, activeResume) {
       state.activeResume = activeResume;
     },
     updateResumes(state, resumes) {
       state.resumes = resumes;
-      // console.dir(toRaw(state.resumes));
     },
   },
   actions: {
     updateActiveVacancy({ commit }, activeVacancy) {
       commit("updateActiveVacancy", activeVacancy);
+    },
+    updateActiveResumeId({ commit }, activeResumeId) {
+      commit("updateActiveResumeId", activeResumeId);
     },
     updateActiveResume({ commit }, activeResume) {
       commit("updateActiveResume", activeResume);
